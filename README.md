960e1ce7c74d63a3671a6d2c0321061e90b63fe3
nvphong <nvphong@gmail.com>
2021-10-09 09:19:57 +0000

    0.1.2

---
+++ README.md
@@ -0,0 +1,10 @@
+# Intro
+
+It is inspired by framework from [nlp.js](https://github.com/axa-group/nlp.js)
+
+
+# Releases
+
+## 0.1.2
+
+* Added `core-loader`
---
+++ core-loader/dock.js
@@ -0,0 +1,107 @@
+const fs = require('fs');
+const path = require('path');
+const containerBootstrap = require('./container-bootstrap');
+
+class Dock {
+  constructor() {
+    this.containers = {};
+  }
+
+  getContainer(name) {
+    return this.containers[name || 'default'];
+  }
+
+  get(name) {
+    const container = this.getContainer();
+    if (container) {
+      return container.get(name);
+    }
+    return undefined;
+  }
+
+  async createContainer(
+    name,
+    settings,
+    srcMustLoadEnv,
+    preffix,
+    parent,
+    pipelines
+  ) {
+    const mustLoadEnv = srcMustLoadEnv === undefined ? true : srcMustLoadEnv;
+    if (typeof name !== 'string') {
+      settings = name;
+      name = '';
+    }
+    if (!settings) {
+      if (name === 'default' || name === '') {
+        settings = 'conf.json';
+      } else {
+        settings = path.join(name, 'conf.json');
+        if (!fs.existsSync(settings)) {
+          settings = `${name}_conf.json`;
+        }
+      }
+    }
+    if (!this.containers[name]) {
+      const container = containerBootstrap(
+        settings,
+        mustLoadEnv,
+        undefined,
+        preffix,
+        pipelines,
+        parent
+      );
+      container.name = name;
+      this.containers[name] = container;
+      container.dock = this;
+      await container.start();
+      if (container.childs) {
+        await this.buildChilds(container);
+      }
+    }
+    return this.containers[name];
+  }
+
+  async buildChilds(container) {
+    if (container && container.childs) {
+      const keys = Object.keys(container.childs);
+      const childs = {};
+      for (let i = 0; i < keys.length; i += 1) {
+        const settings = container.childs[keys[i]];
+        settings.isChild = true;
+        if (!settings.pathPipeline) {
+          settings.pathPipeline = `${keys[i]}_pipeline.md`;
+        }
+        childs[keys[i]] = await this.createContainer(
+          keys[i],
+          settings,
+          false,
+          keys[i],
+          container,
+          container.childPipelines
+            ? container.childPipelines[keys[i]]
+            : undefined
+        );
+      }
+      container.childs = childs;
+    }
+  }
+
+  async terraform(settings, mustLoadEnv = true) {
+    const defaultContainer = await this.createContainer(
+      'default',
+      settings,
+      mustLoadEnv,
+      ''
+    );
+    return defaultContainer;
+  }
+
+  start(settings, mustLoadEnv = true) {
+    return this.terraform(settings, mustLoadEnv);
+  }
+}
+
+const dock = new Dock();
+
+module.exports = dock;
